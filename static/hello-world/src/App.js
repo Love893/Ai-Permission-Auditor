1ec9e56fd902745a18a216df00917768d806814f
Shivansh Pandey <shivanshp@clovity.com>
2025-08-27 01:53:10 +0530

    send data on button click added

--- static/hello-world/src/App.js
+++ static/hello-world/src/App.js
@@ -2,31 +2,20 @@ import React, { useEffect, useState } from "react";
 import { invoke, view } from "@forge/bridge";
 
 export default function App() {
-  const [loading, setLoading] = useState(true);
   const [result, setResult] = useState(null);
-  const [error, setError] = useState(null);
-  const [expandedRow, setExpandedRow] = useState(null); // Track which row is expanded
   const [cloudId, setCloudId] = useState("");
 
   useEffect(() => {
     async function fetchAudit() {
       try {
-        setLoading(true);
         const context = await view.getContext()
     const cloudId = context.cloudId;
     setCloudId(cloudId);
         const result = await invoke("runAudit" ,  { cloudId });
         setResult(result)
         // console.log("Result:", result);
-        if (result.success) {
-          setAuditData(result.data);
-        } else {
-          setError(result.error);
-        }
       } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
+        console.log("error",err);
       }
     }
 
@@ -37,8 +26,7 @@ export default function App() {
 
 async function sendtodb() {
   const datasendtodb =await invoke("sendHardcodedProjectsToSQS", { result });
-  console.log("SQS data sent",datasendtodb);
-  
+  console.log("SQS data sent ", datasendtodb);  
 }
 
 return (
